feca553f83a7fbaa812427ee55e54b4fd722fd4f
andres <gokutanve@gmail.com>
2025-03-16 23:48:25 -0400

    cleaned and optimized some functions, added the logic of adding body when eating, modified function move to move either body or head

--- lib/snake.py
+++ lib/snake.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 class Table:
     def __init__(self):
         self.table = [[0 for _ in range(9)] for _ in range(9)]
@@ -19,24 +20,61 @@ class Table:
     def valid_position(self, row, col):
         return 0 <= int(col) <= 8 and 0 <= int(row) <= 8
     
-    def get_snake_position(self):
+    def get_element_position(self, element):
         positions = []
         for i in range(9):
             for k in range(9):
-                if self.table[i][k] == 1:
+                if self.table[i][k] == element:
                     positions.append([i,k])
         return positions
+    
+    def random_position(self):
+        x = random.randint(0, 8)
+        y = random.randint(0, 8)
+        while self.table[x][y] != 0:
+            x = random.randint(0, 8)
+            y = random.randint(0, 8) 
+        return x, y
+
+    def insert_apple(self):
+        x, y = self.random_position()
+        self.insert_at(x , y , 2)
+
+    def clear(self):
+        apple_position = self.get_element_position(2)
+        if apple_position:
+            x , y = apple_position[0]
+            self.table = [[0 for _ in range(9)] for _ in range(9)]
+            self.table[x][y] = 2
+        else:
+            self.table = [[0 for _ in range(9)] for _ in range(9)]
+
+    
+    def update_table(self, positions):
+        self.clear()
+
+        for key in positions:
+            if key == 'head':
+                self.insert_at(*positions[key], 1)
+            else:
+                for position in positions['body']:
+                    print(positions)
+                    self.insert_at(*position, 3)
 
 
 
 
 
-
-class Snake:
+class Snake(pygame.sprite.Sprite):
     def __init__(self):
-        self.head = 1
+        super().__init__()
+
+        self.image = pygame.image.load('lib/snake_head.png')
+        self.rect = self.image.get_rect()
         self.direction = [0,1]
-        pass
+        self.positions = { 'head': [0,0],
+                            'body': []
+        }
 
     def update_direction(self, key_pressed):
 
@@ -54,21 +92,38 @@ class Snake:
             self.direction[0] = 1
 
     def move(self, table):
-        positions = table.get_snake_position()
-        print(positions)
-        row = positions[0][0]
-        col = positions[0][1]
+        old_head_position_x, old_head_position_y = self.positions['head'][0], self.positions['head'][1]
+        old_body_position_x, old_body_position_y = old_head_position_x, old_head_position_y
         if self.direction == [0,1]:
-            table.insert_at(row, col + 1, 1)
-            table.delete_at(row, col)
+            self.positions['head'] = [old_head_position_x, old_head_position_y + 1]
         if self.direction == [0,-1]:
-            table.insert_at(row, col - 1, 1)
-            table.delete_at(row, col)
+            self.positions['head'] = [old_head_position_x, old_head_position_y - 1]
         if self.direction == [1,0]:
-            table.insert_at(row + 1, col, 1)
-            table.delete_at(row, col)
+            self.positions['head'] = [old_head_position_x + 1, old_head_position_y]
         if self.direction == [-1,0]:
-            table.insert_at(row - 1, col, 1)
-            table.delete_at(row, col)
+            self.positions['head'] = [old_head_position_x - 1, old_head_position_y]
 
-            
+        for i in range(len(self.positions['body'])):
+            temp_x, temp_y = self.positions['body'][i]
+            self.positions['body'][i] = [old_body_position_x, old_body_position_y]
+            old_body_position_x, old_body_position_y = temp_x, temp_y
+        
+        table.update_table(self.positions)
+
+
+    def node(self, table):
+        if self.positions['body'] == []:
+            x, y = [x + y for x, y in zip(self.direction, self.positions['head'])]
+            self.positions['body'].append([x,y])
+        else:
+            x, y = [x + y for x, y in zip(self.direction, self.positions['body'][-1])]
+            self.positions['body'].append([x,y])
+
+class Apple(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load('lib/apple.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.position = [[x/34, y/34]]
+
--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import pygame
 from lib.player import Player
 from lib.snake import Snake
 from lib.snake import Table
+from lib.snake import Apple
 from sys import exit
 
 pygame.init()
 
-snake = Snake()
+snake = pygame.sprite.GroupSingle()
+apples = pygame.sprite.GroupSingle()
+snake.add(Snake())
 table = Table()
 x = 0
 y = 0
+apple_x = 0
+apple_y = 0
 table.insert_at(x, y, 1)
-print(table.table)
-dt = 0.1
 px_of_square = 306
 keep_game = True
 last_key_pressed = pygame.K_DOWN
@@ -20,9 +23,8 @@ last_key_pressed = pygame.K_DOWN
 clock = pygame.time.Clock()
 last_time_moved = 0
 snake_speed = 700
-snake_head_img = pygame.image.load('lib/snake_head.jpeg')
-snake_head_img = pygame.transform.scale(snake_head_img, (snake_head_img.get_width() * 1/10,
-                                        snake_head_img.get_height() * 1/10))
+spawn_apple_speed = 3000
+last_eaten_time = 0
 
 
 screen = pygame.display.set_mode((px_of_square,px_of_square))
@@ -33,20 +35,39 @@ while keep_game:
     current_time = pygame.time.get_ticks()
     keys = pygame.key.get_pressed()
     screen.fill((0,0,0))
-    screen.blit(snake_head_img, (x,y))
+    snake.draw(screen)
+    apples.draw(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
     last_key_pressed = Player.get_movement(last_key_pressed ,keys)
-    snake.update_direction(last_key_pressed)
+    snake.sprite.update_direction(last_key_pressed)
+
     if current_time - last_time_moved > snake_speed:
         last_time_moved = current_time
-        snake.move(table)
-        x = table.get_snake_position()[0][0] * 34
-        y = table.get_snake_position()[0][1] * 34
-    print(snake.direction)
+        snake.sprite.move(table)
+
+        x = snake.sprite.positions['head'][0] * 34
+        y = snake.sprite.positions['head'][1] * 34
+        snake.sprite.rect.topleft = (x, y)
+        
+        if table.collision:
+            keep_game = False  
+
+        if apples.sprite and pygame.Rect.colliderect(apples.sprite.rect, snake.sprite.rect):
+            apples.sprite.kill()
+            snake.sprite.node(table)
+
+    if current_time - last_eaten_time > spawn_apple_speed and table.get_element_position(2) == []:
+            table.insert_apple()
+            apple_x = table.get_element_position(2)[0][0] * 34
+            apple_y = table.get_element_position(2)[0][1] * 34
+            apples.add(Apple(apple_x, apple_y))
+    
+
 
     pygame.display.flip()
     clock.tick(60)
